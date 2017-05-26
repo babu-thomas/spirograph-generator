6079afd1e6a78b27d1cdee98e6c6434197737351
Babu Thomas <babuthomas138@gmail.com>
2017-05-27 01:54:15 +0530

    Combine paths the correct way

--- spirograph-generator/Program.fs
+++ spirograph-generator/Program.fs
@@ -1,11 +1,10 @@
 ﻿open System.Drawing
+open System.IO
 
 [<EntryPoint>]
 let main argv = 
     let bitmap = new Bitmap(16, 16)
-
-    let path = __SOURCE_DIRECTORY__ + @"\"
-
-    bitmap.Save(path + "bitmap.png", Imaging.ImageFormat.Png)
+    let path = Path.Combine(__SOURCE_DIRECTORY__, "bitmap.png")
+    bitmap.Save(path, Imaging.ImageFormat.Png)
     printfn "Hello, World"
     0
