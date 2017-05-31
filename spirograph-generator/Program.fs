06ab74b6f5bb13f4e212f1e7c9bc135c0b1fd786
Babu Thomas <babuthomas138@gmail.com>
2017-05-31 23:47:18 +0530

    Implement moving

--- spirograph-generator/Program.fs
+++ spirograph-generator/Program.fs
@@ -1,12 +1,20 @@
 ﻿open System.Drawing
 open System.IO
+open System
 
 type Plotter = {
     position: int * int
     color: Color
     direction: float
-    bitmap: Bitmap
-    }
+    bitmap: Bitmap }
+
+let drawLine (endX, endY) plotter =
+    use graphics = Graphics.FromImage(plotter.bitmap)
+    use pen = new Pen(plotter.color)
+    let startX, startY = plotter.position
+    graphics.DrawLine(pen, startX, startY, endX, endY)
+    let updatedPlotter = { plotter with position = (endX, endY) }
+    updatedPlotter
 
 let turn amount plotter =
     let newDir = plotter.direction + amount
@@ -15,17 +23,27 @@ let turn amount plotter =
     turned
 
 let move distance plotter =
-    plotter
+    let angle = plotter.direction
+    let rads = (angle - 90.0) * Math.PI/180.0
+    let startX, startY = plotter.position
+    let endX = (float startX) + (float distance) * cos rads
+    let endY = (float startY) + (float distance) * sin rads
+    let moved = drawLine (int endX, int endY) plotter
+    printfn "%A" moved
+    moved
 
 [<EntryPoint>]
 let main argv = 
-    use bitmap = new Bitmap(16, 16)
-    use graphics = Graphics.FromImage(bitmap)
-    use redPen = new Pen(Brushes.Red)
+    use bitmap = new Bitmap(100, 100)
+
+    let initialPlotter = {
+        position = (0, 0)
+        color = Color.Goldenrod
+        direction = 0.0
+        bitmap = bitmap }
     
-    graphics.DrawRectangle(redPen, Rectangle(5, 5, 10, 10))
+    drawLine (50, 50) initialPlotter |> ignore
 
     let path = Path.Combine(__SOURCE_DIRECTORY__, "bitmap.png")
     bitmap.Save(path, Imaging.ImageFormat.Png)
-    printfn "Hello, World"
     0
